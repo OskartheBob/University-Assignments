db8b117faecf83c9528e2773fbaaa4eb10f23d71
Oskar <oskarsokol.1@gmail.com>
2025-02-08 16:47:14 +0000

    2

--- Assignment_2/src/Main.java
+++ Assignment_2/src/Main.java
@@ -1,6 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class Main {
-
     public static void main(String[] args){
+        input();
+    }
 
+    public static void input(){
+        String expression = JOptionPane.showInputDialog("Please enter an infix numerical expression between 3 and 20 characters:");
     }
 }
