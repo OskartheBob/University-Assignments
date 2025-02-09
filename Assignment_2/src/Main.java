f4d10660fa89b8da16aadcb5b20b3a83587f3310
Oskar <oskarsokol.1@gmail.com>
2025-02-09 16:39:46 +0000

    2

--- Assignment_2/src/Main.java
+++ Assignment_2/src/Main.java
@@ -1,15 +1,26 @@
 import javax.swing.JOptionPane;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         input();
+        postFix();
     }
 
-    public static void input(){
-        String expression = JOptionPane.showInputDialog("Please enter an infix numerical expression between 3 and 20 characters:");
-
-        switch(expression){
-            case 
+    public static void input() {
+        while(true) {
+            String expression = JOptionPane.showInputDialog("Please enter an infix numerical expression between 3 and 20 characters:");
+            if (expression.length() < 3 || expression.length() > 20) {
+                JOptionPane.showMessageDialog(null, "The expression needs to be between 3 and 20 characters long", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
+            } else if (!expression.matches("[0-9+*/^()-]+")) {
+                JOptionPane.showMessageDialog(null, "Only the following characters are valid: +,-,*,/,^,(), and numbers from 0-9", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
+            }
+            else {
+                break;
+            }
         }
+    }
+
+    public static void postFix() {
+
     }
 }
