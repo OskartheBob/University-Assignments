5395f1b50ac0126336c498318aad8b7b6f2c72f3
Oskar <oskarsokol.1@gmail.com>
2025-02-14 20:15:55 +0000

    AHHHHH!

--- Assignment_2/src/ExpressionHandling.java
+++ Assignment_2/src/ExpressionHandling.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
+import java.lang.*;
+import java.util.*;
 
 public class ExpressionHandling {
     private String expression;
+    private char[] expressionChar;
+    private char currentChar;
+    private int operatorValue = 0;
+
     public void input() {
 
 
         while(true) {
             char prevChar = '+';
-            char currentChar = '+';
             int noOpen = 0;
             int noClosed = 0;
             boolean isValid = true;
@@ -20,7 +25,7 @@ public class ExpressionHandling {
             } else if(expression.charAt(0) == ')' || expression.charAt(expression.length() - 1) == '('){
                 JOptionPane.showMessageDialog(null, "Invalid expression", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
             } else {
-                char[] expressionChar = expression.toCharArray();
+                expressionChar = expression.toCharArray();
                 for(int i = 0; i < expression.length(); i++) {
                     currentChar = expressionChar[i];
                     if("+*/^-)".indexOf(currentChar) != -1 && "+*/^-(".indexOf(prevChar) != -1){
@@ -46,7 +51,6 @@ public class ExpressionHandling {
                     JOptionPane.showMessageDialog(null, "Invalid expression", "ERROR!", JOptionPane.INFORMATION_MESSAGE);
                     isValid = false;
                 }
-                System.out.println(noClosed+","+ noOpen);
                 if(isValid){
                     break;
                 }
@@ -55,7 +59,91 @@ public class ExpressionHandling {
         }
     }
 
-    public void postfix(){
-
+    public int operatorValue(char operator){
+        switch(operator) {
+            case '^':
+                return operatorValue = 3;
+            case '*', '/':
+                return operatorValue = 2;
+            case '+', '-':
+                return operatorValue = 1;
+            default:
+                return operatorValue = 0;
+        }
     }
+
+    public void postfix(){
+        String output = "";
+        ArrayStack arrayStack = new ArrayStack();
+        char prevChar = '0';
+        for(int i = 0 ; i < expression.length(); i++){
+            currentChar = expressionChar[i];
+            if(Character.isDigit(currentChar)){
+                output += currentChar;
+            } else if(arrayStack.isEmpty() || operatorValue(currentChar) > operatorValue((char)arrayStack.top()) || (char)arrayStack.top() == '(') {
+                arrayStack.push(currentChar);
+
+            }else{
+                while(!arrayStack.isEmpty() && operatorValue((char)arrayStack.top()) >= operatorValue(currentChar)) {
+                    if ((char) arrayStack.top() == '(' || (char) arrayStack.top() == ')') {
+                        break;
+                    }
+                    output += arrayStack.pop();
+                }
+                if(currentChar != ')') {
+                    arrayStack.push(currentChar);
+                }
+            }
+
+            if (currentChar == ')') {
+                while (!arrayStack.isEmpty() && (char)arrayStack.top() != '(') {
+                    output += arrayStack.pop();
+                }
+                if (!arrayStack.isEmpty() && (char) arrayStack.top() == '(') {
+                    arrayStack.pop();
+                }
+            }
+        }
+        while (!arrayStack.isEmpty()) {
+            output += arrayStack.pop();
+        }
+        System.out.println(output);
+    }
+    public void postEval(){
+        ArrayStack arrayStack = new ArrayStack();
+        for(int i = 0; i < expression.length(); i++){
+            currentChar = expressionChar[i];
+            if(Character.isDigit(currentChar)){
+                arrayStack.push(currentChar);
+            }else{
+                float temp;
+                switch(currentChar){
+                    case '+':
+                        temp = ((Number)arrayStack.pop()).floatValue();
+                        arrayStack.push(temp + ((Number)arrayStack.pop()).floatValue());
+                        break;
+                    case '-':
+                        temp = (Float)arrayStack.pop();
+                        arrayStack.push(temp - (float)arrayStack.pop());
+                        break;
+                    case '*':
+                        temp = (float)arrayStack.pop();
+                        arrayStack.push(temp * (float)arrayStack.pop());
+                        break;
+                    case '/':
+                        temp = (float)arrayStack.pop();
+                        arrayStack.push(temp / (float)arrayStack.pop());
+                        break;
+                    case '^':
+                        temp = (float)arrayStack.pop();
+                        arrayStack.push(Math.pow(temp, (float)arrayStack.pop()));
+                        break;
+
+                }
+
+            }
+        }
+        System.out.println(arrayStack.pop());
+    }
+
 }
--- Assignment_2/src/Main.java
+++ Assignment_2/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ExpressionHandling expression = new ExpressionHandling();
         expression.input();
+        expression.postfix();
+        expression.postEval();
     }
 }
